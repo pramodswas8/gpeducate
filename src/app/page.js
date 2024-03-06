c7401899cabcff16c6538b48c2e7c9c4dd9b3d9c
pramodswas8 <45346510+pramodswas8@users.noreply.github.com>
2024-03-06 16:31:08 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import HomePage from "./_home";
+
+export const metadata = {
+  title: "Home",
+};
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <HomePage />
+      </div>
+    </main>
+  );
+}
